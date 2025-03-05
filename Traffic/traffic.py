69bc4fb394b7d14b986d67e04ba9430c4c8c5f23
merle <merle.idr@gmail.com>
2025-03-05 10:22:54 +0000

    Add files via upload

--- Traffic/traffic.py
+++ Traffic/traffic.py
@@ -4,42 +4,57 @@ import numpy as np
 
 N = int(input('length of road: '))
 ITERATIONS = int(input('iterations: '))
-CARDENSITY = float(input('car density: '))
+CARDENSITY = float(input('car density: '))#as a decimal
 
 class Traffic:
     def __init__(self, length, density, iterations):
         self.length = length
         self.density = density
         self.iterations = iterations
-        self.num_cars = round(self.density * self.length)
+        self.num_cars = round(self.density * self.length) #ensure num_cars is an integer
         self.gen_road(self.num_cars)
 
 
-    def gen_road(self, cars_on_road): #pass argument so can be used for steady state calculations
-        self.road = [0]*self.length
+    def gen_road(self, cars_on_road): #pass cars_on_road so can be used for steady state calculations
+        '''
+        Generate a road of 1s and 0s by randomly allocating the positions of the 1s
+        '''
+        self.road = [0]*self.length #create list of zeros
         indices = random.sample(range(self.length), cars_on_road) 
         for i in indices:
-            self.road[i] = 1
+            self.road[i] = 1 #randomly change certain zeros to one by generating a set of indexes
     
   
     def update_road(self):
+        '''
+        Runs the road moving process and updates the road
+        '''
         road = self.road[:]
-        states = [] #all the roads, so i can plot them !
+        states = [] #all the roads, so i can plot them 
         average_speeds = []
 
-        for _ in range(self.iterations):
-            new_road = road[:]
+        for _ in range(self.iterations): #running the movement process 'iteration' number of times
+            new_road = road[:] #copy the road
             moves_this_timestep = 0
 
-            for i in range(self.length):
+            for i in range(self.length): #implement 
                 next_pos = (i + 1) % self.length
-                if road[i] == 1 and road[next_pos] == 0:
-                    new_road[i] = 0
-                    new_road[next_pos] = 1
-                    moves_this_timestep += 1
+                prev_pos = (i - 1) % self.length
 
-            road = new_road[:]
-            states.append(road)
+                if road[i] == 1:                    
+                    if road[next_pos] == 1:
+                        new_road[i] = 1
+                    else:
+                        new_road[i] = 0
+                        moves_this_timestep += 1
+                elif road[i] == 0:
+                    if road[prev_pos] == 1:
+                        new_road[i] = 1
+                    else:
+                        new_road[i] = 0
+
+            road = new_road[:] #copy the new road
+            states.append(road) #saving every road after every iteration so they can be plotted
 
             #average speed
             avg_speed = moves_this_timestep / self.num_cars if self.num_cars > 0 else 0
@@ -50,6 +65,9 @@ class Traffic:
     
 
 def plot_road(states):   
+    '''
+    Graphically representing the cars
+    '''
     road_array = np.array(states)
     fig, ax = plt.subplots(figsize=(5, 6))
     cax = ax.imshow(road_array, cmap='Blues', vmin=0, vmax=1, aspect='auto', origin='lower')
@@ -58,12 +76,16 @@ def plot_road(states):
     plt.show()
 
 def steady_state(N):
+        '''
+        Plotting the steady state speed against the number of cars on the road for a fixed length road
+        '''
         steady_speeds = []
         for i in range(1, N+1):
             sim = Traffic(N, i/N, N)
-            _, average_speeds = sim.update_road()
-            steady_speeds.append(average_speeds[-1])
+            _, average_speeds = sim.update_road() 
+            steady_speeds.append(average_speeds[-1]) #taking the last speed after running the update_road algorithm N times
         
+        #Creating the plot
         x = np.arange(N)
         y = np.array(steady_speeds)
         plt.title('Steady State Speed against Number of Cars on Road')
@@ -73,7 +95,7 @@ def steady_state(N):
         plt.show()
 
 if __name__ == '__main__':
-    simulation = Traffic(N, CARDENSITY, ITERATIONS)
+    simulation = Traffic(N, CARDENSITY, ITERATIONS) #takes user input
     states, average_speeds = simulation.update_road()
 
     for i, speed in enumerate(average_speeds, start=1):
